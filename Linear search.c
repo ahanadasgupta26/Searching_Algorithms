989607709126094f2c52d03770c0b1228734d445
Ahana Dasgupta <161189328+ahanadasgupta26@users.noreply.github.com>
2025-03-13 22:07:30 +0530

    Update Linear search.c

--- Linear search.c
+++ Linear search.c
@@ -1,1 +1,40 @@
-
+#include<stdio.h>
+int a[10];
+void getdata()
+{
+	int i;
+	printf("ENTER 10 ELEMENTS: ");
+	for(i=0;i<10;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+}
+void search(int key)
+{
+	int i;
+	int flag=-1;
+	for(i=0;i<10;i++)
+	{
+		if(a[i]==key)
+		{
+			flag=i;
+		}
+	}
+	if(flag!=-1)
+	{
+		printf("DATA FOUND IN POSITION %d,",flag+1);
+	}
+	else
+	{
+		printf("DATA NOT FOUND...");
+	}
+}
+int main()
+{
+	int key;
+	getdata();
+	printf("ENTER THE ELEMENT WHICH YOU WANT TO FIND ");
+	scanf("%d",&key);
+	search(key);
+	return 0;
+}
